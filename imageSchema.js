d065de762dbf9e9a731758b2dbfcd6bc51072db6
Mohit-code-dot <mohit@workinxdigital.us>
2024-08-28 19:37:40 +0530

    Delete imageSchema.js

--- imageSchema.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-    imgpath:String
-});
-const imageModel = new mongoose.model("image",userSchema);
-module.exports = imageModel; 
